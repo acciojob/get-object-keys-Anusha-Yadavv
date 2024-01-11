47e5da1cb630453e5323abebce7121c13298a5e2
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-01-11 22:52:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Creating the student object with a 'name' property
+const student = {
+  name: "John Doe"
+};
+
+// Adding a getKeys() method to the Object prototype
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Testing the getKeys() method on the student object
+const keysArray = student.getKeys();
+console.log(keysArray); // Output: ['name']
